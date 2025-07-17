3104649a6ea9f65b0da73a83fcf3d802857a0d27
Aaron Thompson King <thompsonking1991@gmail.com>
2025-07-17 16:05:48 -0700

    Create webpack.config.js

---
+++ 0x04-TypeScript/task_0/task_1/js/task_1/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './js/main.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // We'll create this next
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    open: true,
+    port: 8080,
+  },
+};
